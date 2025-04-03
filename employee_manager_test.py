7d5c9f1bbea8f29a06c51f902e091bb06f1b9431
ervinfabian <ervinfab@gmail.com>
2025-04-03 17:59:41 +0300

    hopefuly final version

--- employee_manager_test.py
+++ employee_manager_test.py
@@ -6,7 +6,7 @@ import datetime
 def test_not_team_leader_salary():
     # Check an employee’s salary who is not a team leader 
     # whose hire date is 10.10.1998 and his base salary is 
-    # 1000$. Make sure the returned value is 3000$ (1000$ + 20 X 100$).
+    # 1000$. Make sure the returned value is 3700$ (1000$ + 20 X 100$).
 
     rm = relations_manager.RelationsManager()
     em = employee_manager.EmployeeManager(rm)
@@ -15,7 +15,7 @@ def test_not_team_leader_salary():
     employee = rm.get_employee_by_id(7)
     output = em.calculate_salary(employee)
     # expected salary is 1000 + 27 x 100 = 3700
-    assert output == 3700
+    assert output == 3700, "The salary of the employee is not equal to the expected value"
 
 def test_team_leader_salary():
     # Check an employee’s salary who is a team leader 
@@ -34,7 +34,7 @@ def test_team_leader_salary():
     print(rm.get_team_members(employee))
     output = em.calculate_salary(employee)
     # expected salary is 2000 + 17 x 100 + 3 x 200 = 4300
-    assert output == 4300
+    assert output == 4300, "The salary of the employee is not equal to the expected value"
 
 def test_calculate_salary_send_email():
     # Make sure that when you calculate the salary and send an email notification, 
@@ -52,4 +52,4 @@ def test_calculate_salary_send_email():
     employee_informations = ()
 
     employee_informations = em.calculate_salary_and_send_email(rm.get_employee_by_id(8))
-    assert employee_informations[0] == "employee" and employee_informations[1] == "eight" and employee_informations[2] == 4300
+    assert employee_informations[0] == "employee" and employee_informations[1] == "eight" and employee_informations[2] == 4300, "the email sender and salary calculator function is not working properly"
--- employee_relations_manager_test.py
+++ employee_relations_manager_test.py
@@ -24,11 +24,15 @@ def test_myrta_and_jettie_in_john_doe_team():
     rm = relations_manager.RelationsManager()
     employee = rm.get_employee_by_name("John", "Doe") 
     team_members = rm.get_team_members(employee)
+    number_of_correct_employees = 0
     for tm in team_members:
         emp = rm.get_employee_by_id(tm)
-        if emp.first_name == "Myrta" or emp.first_name == "Jettie":
-            if emp.last_name == "Torkelson" or emp.last_name == "Lynch":
-                output = True
+        if emp.first_name == "Myrta" and emp.last_name == "Torkelson":
+            number_of_correct_employees = number_of_correct_employees + 1
+        if emp.first_name == "Jettie" and emp.last_name == "Lynch":
+            number_of_correct_employees = number_of_correct_employees + 1
+    if number_of_correct_employees == 2:
+        output = True
 
     assert output ==  True, "Jettie Lynch and Myrta Torkelson are not John Doe's team members"
 
@@ -37,17 +41,17 @@ def test_myrta_and_jettie_in_john_doe_team():
 def test_andre_not_in_doe_team():
     # Make sure that Tomas Andre is not John Doe’s team member.
 
-    output = True
+    output = False
     rm = relations_manager.RelationsManager()
     doe = rm.get_employee_by_name("John", "Doe")
     team_members = rm.get_team_members(doe)
     for tm in team_members:
         emp = rm.get_employee_by_id(tm)
         if emp.first_name == "Tomas" and emp.last_name == "Andre":
-            output = False
-    assert output == True, "Tomas Andre is John Doe's team member"
+            output = True
+    assert output == False, "Tomas Andre is John Doe's team member"
     
-def test_gretchen_watford_salary_equals_4000():
+def test_gretchen_watford_base_salary_equals_4000():
     # Check if Gretchen Walford’s base salary equals 4000$.
 
     output = False
@@ -60,24 +64,24 @@ def test_gretchen_watford_salary_equals_4000():
 def test_no_jude_overcash_in_database():
     # Make sure that Jude Overcash is not stored in the database.
 
-    output = True
+    output = False
     rm = relations_manager.RelationsManager()
     employees = rm.get_all_employees()
     for e in employees:
         if e.first_name == "Jude" and e.last_name == "Overcash":
-            output = False
-    assert output == True, "Jude overcash is stored in the database"
+            output = True
+    assert output == False, "Jude overcash is stored in the database"
 
 def test_if_tomas_andre_is_not_team_leader():
     # Make sure Tomas Andre is not a team leader. 
     # Check what happens if you try to retrieve his team members.
 
-    output = True
+    output = False
     rm = relations_manager.RelationsManager()
     tomas_andre = rm.get_employee_by_name("Tomas", "Andre")
     if rm.is_leader(tomas_andre):
-        output = False
-    assert output == True
+        output = True
+    assert output == False
     assert rm.get_team_members(tomas_andre) is None, "Tomas Andre is a team leader"
 
     
